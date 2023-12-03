0405472c42b692ba0fecb0649a0a9830fd3b2bda
frontendsprint <frontendsprint@gmail.com>
2023-12-04 01:24:14 +0200

    commit

---
+++ javascript/01-basic/t38/t38.js
@@ -0,0 +1,10 @@
+//Word Blanks
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Змініть код лише під цим рядком
+const wordBlanks = "This " + myNoun + " was " + myAdjective + " and it " + myVerb + " so " + myAdverb + "."; // Змініть цей рядок
+// Змініть код лише над цим рядком
