906d8cef7ead06a66fe93bd76c92deb995c8a755
frontendsprint <frontendsprint@gmail.com>
2023-12-06 17:47:16 +0200

    commit

---
+++ javascript/01-basic/t88/t88.js
@@ -0,0 +1,9 @@
+//Add New Properties to a JavaScript Object
+
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  myDog.bark = "woof";
