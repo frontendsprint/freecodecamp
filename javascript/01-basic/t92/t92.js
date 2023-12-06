0ba3481af5d4097f773473762ff5dfc95a6d06b9
frontendsprint <frontendsprint@gmail.com>
2023-12-06 18:43:19 +0200

    commit

---
+++ javascript/01-basic/t92/t92.js
@@ -0,0 +1,26 @@
+//Manipulating Complex Objects
+
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man 2",
+      "release_year": 1974,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      
+    }
+  ];
