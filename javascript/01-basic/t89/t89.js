7bccc5f26880f498e697dc15f9f0402f5e019800
frontendsprint <frontendsprint@gmail.com>
2023-12-06 17:55:14 +0200

    commit

---
+++ javascript/01-basic/t89/t89.js
@@ -0,0 +1,13 @@
+//Using Objects for Lookups
+
+// Setup
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+  };
+  
+  // Only change code below this line
+  delete myDog.tails;
