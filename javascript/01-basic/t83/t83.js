3f63516e5ef9a955acd580ecd44cab6a2a86f1f5
frontendsprint <frontendsprint@gmail.com>
2023-12-06 09:49:05 +0200

    commit

---
+++ javascript/01-basic/t83/t83.js
@@ -0,0 +1,10 @@
+//Build JavaScript Objects
+
+const myDog = {
+    // Only change code below this line
+    name: "Rex",
+    legs:4,
+    tails:1,
+    friends: []
+    // Only change code above this line
+  };
