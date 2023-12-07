019114b0bf62f1283af8c542e86685507f2ae610
frontendsprint <frontendsprint@gmail.com>
2023-12-07 11:56:51 +0200

    commit

---
+++ 03-frontend-development-libraries/04-react/t02/t02.js
@@ -0,0 +1,13 @@
+//Create a Complex JSX Element
+
+const JSX = (
+    <div>
+      <h1>Hello</h1>
+      <p>World</p>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+);
