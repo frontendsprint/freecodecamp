bea69733559297e9e4b99e728958884fe076365b
frontendsprint <frontendsprint@gmail.com>
2023-12-11 15:17:50 +0200

    commit

---
+++ 02-javascript-algorithms/05-basic-data-structures/t06.js
@@ -0,0 +1,8 @@
+//Add Items Using splice()
+
+function htmlColorNames(arr) {
+    arr.splice(0,2,'DarkSalmon','BlanchedAlmond');
+    return arr;
+}
+
+console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
