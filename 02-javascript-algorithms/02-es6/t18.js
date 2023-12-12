3c94046a14a66afb4ade02fd7fc0dc117a970ac1
frontendsprint <frontendsprint@gmail.com>
2023-12-12 16:23:25 +0200

    commit

---
+++ 02-javascript-algorithms/02-es6/t18.js
@@ -0,0 +1,9 @@
+//Use class Syntax to Define a Constructor Function
+
+class Vegetable {
+    constructor(name){
+        this.name = name;
+    }
+}
+const carrot = new Vegetable('carrot');
+console.log(carrot.name);
