ab6ec4998a03bfcdd1c04a2624417f77ec0fe865
frontendsprint <frontendsprint@gmail.com>
2023-12-13 13:56:49 +0200

    commit

---
+++ 02-javascript-algorithms/02-es6/t28.js
@@ -0,0 +1,15 @@
+//Handle a Fulfilled Promise with then
+
+const makeServerRequest = new Promise((resolve, reject) => {
+		let responseFromServer = true;
+
+		if(responseFromServer) {
+				resolve("We got the data");
+		} else {
+				reject("Data not received");
+		}
+});
+
+makeServerRequest.then(result => {
+		console.log(result);
+});
