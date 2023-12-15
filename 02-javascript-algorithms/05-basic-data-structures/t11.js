680802358891cb0f08ae0363e45fbb9b7c731979
frontendsprint <frontendsprint@gmail.com>
2023-12-15 11:45:51 +0200

    commit

--- 02-javascript-algorithms/05-basic-data-structures/t11.js
+++ 02-javascript-algorithms/05-basic-data-structures/t11.js
@@ -1,2 +1,16 @@
 //Iterate Through All an Array's Items Using For Loops
 
+function filteredArray(arr, elem) {
+		let newArr = [];
+		for (let i = 0; i < arr.length; i++){
+				for (let j = 0; j < arr[i].length; j++){
+						if (arr[i][j] == elem){
+								j = arr[i][j].length;
+								newArr.push(arr.slice(arr[i], arr[i] + 1));
+						}
+				}
+		}
+		return newArr;
+}
+
+console.log(filteredArray([[10, 8, 3], [14, 6, 23], [3, 18, 6]], 18));
