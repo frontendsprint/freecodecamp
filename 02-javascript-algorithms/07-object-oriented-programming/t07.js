bced80fc6e336471fa09b8218d53d53f793c464c
frontendsprint <frontendsprint@gmail.com>
2023-12-19 00:04:01 +0200

    commit

---
+++ 02-javascript-algorithms/07-object-oriented-programming/t07.js
@@ -0,0 +1,9 @@
+//Extend Constructors to Receive Arguments
+
+function Dog(name, color) {
+    this.name = name;
+    this.color = color;
+    this.numLegs = 4;
+  }
+  
+  const terrier = new Dog('Rex','White');
