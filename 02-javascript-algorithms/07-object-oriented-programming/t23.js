0566b461e0c8ecf63441129c5af07172af1eb77b
frontendsprint <frontendsprint@gmail.com>
2023-12-20 11:36:56 +0200

    commit

--- 02-javascript-algorithms/07-object-oriented-programming/t23.js
+++ 02-javascript-algorithms/07-object-oriented-programming/t23.js
@@ -1,2 +1,20 @@
 //Use a Mixin to Add Common Behavior Between Unrelated Objects
 
+let bird = {
+    name: "Donald",
+    numLegs: 2
+  };
+  
+  let boat = {
+    name: "Warrior",
+    type: "race-boat"
+  };
+  
+  let glideMixin = function(obj) {
+    obj.glide = function() {
+      console.log('Glide');
+    }
+  }
+  
+  glideMixin(bird);
+  glideMixin(boat);
---
+++ 02-javascript-algorithms/07-object-oriented-programming/t26.js
@@ -0,0 +1,17 @@
+//Use an IIFE to Create a Module
+
+let funModule = (function() {
+    return {
+        isCuteMixin: function(obj) {
+          obj.isCute = function() {
+             return true;
+           };
+        },
+        singMixin: function(obj) {
+            obj.sing = function() {
+              console.log("Singing to an awesome tune");
+            };
+        }
+  
+    }
+  })();
